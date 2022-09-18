42b3143b7e4ba22723b5b8aa481fdd32fdba891e
sadpu111 <dwme7100@gmail.com>
2022-09-18 13:49:42 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react"
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+/*   useEffect(() =>{
+    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
+    .then((response) => response.json())
+    .then((json) => {
+      setMovies(json.data.movies);
+      setLoading(false)})
+  }, []); */ 
+  const getMovioes = async () => { // 위의 .then과 같은 기능
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
+      ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovioes();
+  }, []);
+  console.log(movies);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<div>
+  {loading ? <h1>Loading...</h1> : <div>{movies.map(movie => <div key={movie.id}>
+    <img src={movie.medium_cover_image} />
+    <h2>{movie.title}</h2>
+    <p>{movie.summary}</p>
+    <ul>
+      {movie.genres === null ? "" : movie.genres.map((genre) => <li key={genre}>{genre}</li>)} 
+    </ul>
+  </div>)}</div>}
+</div>
+  )
 }
 
 export default App;
