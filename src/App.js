fb6584e65d27bf5ad432eb5f1ab8f837e3883ab2
sadpu111 <dwme7100@gmail.com>
2022-09-19 16:30:57 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
-import {useState, useEffect} from "react"
-import Movie from "./Movie";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-/*   useEffect(() =>{
-    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
-    .then((response) => response.json())
-    .then((json) => {
-      setMovies(json.data.movies);
-      setLoading(false)})
-  }, []); */ 
-  const getMovioes = async () => { // 위의 .then과 같은 기능
-    const json = await (
-      await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
-      ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovioes();
-  }, []);
-  console.log(movies);
-  return (
-<div>
-  {loading ? <h1>Loading...</h1> : <div>{movies.map(movie =>
-  (<Movie 
-      coverImg={movie.medium_cover_image}
-      title={movie.title}
-      summary={movie.summary}
-      genres={movie.genres} /> 
-  ))}
-   </div>}
-</div>
-  )
+  return <Router>
+    <Routes>
+      <Route path="/" element={<Home />}>
+      </Route>
+      <Route path="/movie/:id" element={<Detail />}>
+      </Route>
+    </Routes>
+  </Router>
 }
 
 export default App;
---
+++ src/components/Movie.js
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+
+function Movie({id, coverImg, title, summary, genres}) {
+  return (
+  <div>
+    <img src={coverImg} />
+    <h2>
+      <Link to={`/movie/${id}`}>{title}</Link>
+    </h2>
+    <p>{summary}</p>
+    <ul>
+      {genres === null ? "" : genres.map((genre) => <li key={genre}>{genre}</li>)} 
+    </ul>
+</div>)
+}
+// 부모(App.js)에서 프로퍼티 값을 전달받아 함수를 실행하고 해당 내용을 <Movie /> 컴포넌트로 랜더링
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired, // stringd을 가진 array
+}
+
+export default Movie;
